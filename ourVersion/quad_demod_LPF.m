ea008eab9de3d9e32547cceac15dcc0b390e0901
VictorChen2003 <c25victor.chen@afacademy.af.edu@users.noreply.github.com>
2024-12-06 17:01:24 -0700

    Quad Demod LPF Complete

--- ourVersion/quad_demod_LPF.m
+++ ourVersion/quad_demod_LPF.m
@@ -1,4 +1,4 @@
-function [demod_I_LPF, demod_Q_LPF] = quad_demod_LPF(demod_I, demod_Q, NumBeams, FrameSize, WindowLength, filter_coef)
+function [demod_I_LPF, demod_Q_LPF] = quad_demod_LPF(demod_I, demod_Q, NumBeams, filter_coef)
 %  LPF to remove harmonic at twice f, keeping DC component
 %  Convolves data with Filter_coef of length WindowLength
 %
@@ -17,5 +17,15 @@ function [demod_I_LPF, demod_Q_LPF] = quad_demod_LPF(demod_I, demod_Q, NumBeams,
 %       demod_Q:        quadrature component after LPF
 %
 
+% Precompute the output arrays
+demod_I_LPF = zeros(size(demod_I));
+demod_Q_LPF = zeros(size(demod_Q));
+
+% Loop through each signal
+for i = 1:NumBeams
+    % Convolve the current signal with the filter
+    demod_I_LPF(i, :) = conv(demod_I(i, :), filter_coef, 'same');
+    demod_Q_LPF(i, :) = conv(demod_Q(i, :), filter_coef, 'same');
+end
 
 
